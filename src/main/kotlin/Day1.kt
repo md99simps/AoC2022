99b5a320048c0243cb1983c18faf8a32ed92ccb4
md99simps <mdsimps99@gmail.com>
2022-12-07 23:46:06 -0700

    Check in Day 1 solution

---
+++ src/main/kotlin/Day1.kt
@@ -0,0 +1,32 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+    val inputStream = object{}.javaClass.getResourceAsStream("elfcalories.txt")
+
+    var currCalories = 0
+    val calories = IntArray(3)
+
+    inputStream.bufferedReader().forEachLine { line ->
+        val trimmed = line.trim()
+        if (trimmed.isEmpty()) {
+            val first = calories[0]
+            val second = calories[1]
+            val third = calories[2]
+            if (currCalories > first) {
+                calories[0] = currCalories
+                calories[1] = first
+                calories[2] = second
+            } else if (currCalories > second) {
+                calories[1] = currCalories
+                calories[2] = second
+            } else if (currCalories > third) {
+                calories[2] = currCalories
+            }
+            currCalories = 0
+        } else {
+            currCalories += trimmed.toInt()
+        }
+    }
+
+    println("Max calories ${calories.sum()}")
+}
